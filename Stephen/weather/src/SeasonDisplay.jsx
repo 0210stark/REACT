84939e5e9c0de098ba4a2af6e9d42ae699ae4065
Vidit Shah <vidit0210@outlook.com>
2020-07-09 16:00:33 +0530

    pfinished

--- Stephen/weather/src/SeasonDisplay.jsx
+++ Stephen/weather/src/SeasonDisplay.jsx
@@ -18,6 +18,7 @@ const getSeason = (lat, month) => {
     return lat > 0 ? 'winter' : 'summer';
   }
 };
+
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   // const text =
