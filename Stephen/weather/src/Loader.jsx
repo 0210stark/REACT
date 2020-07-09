c73b389d07ad69a1251f133cfff4392585914007
Vidit Shah <vidit0210@outlook.com>
2020-07-09 14:34:30 +0530

    Loading

---
+++ Stephen/weather/src/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div class='ui segment'>
+      <p></p>
+      <div class='ui active dimmer'>
+        <div class='ui loader'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- Stephen/weather/src/index.js
+++ Stephen/weather/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 class App extends React.Component {
   constructor(props) {
@@ -31,7 +32,7 @@ class App extends React.Component {
     if (this.state.lat && !this.state.errorMessage) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading...</div>;
+    return <Loader />;
   }
 }
 ReactDOM.render(<App />, document.getElementById('root'));
