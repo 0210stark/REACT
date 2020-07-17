cde47ca30f9e1709e90f54d0dbf14af4541c64c9
Vidit Shah <vidit0210@outlook.com>
2020-07-18 00:23:30 +0530

    search

--- pics2/src/components/App.jsx
+++ pics2/src/components/App.jsx
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 class App extends Component {
-  getTerm = (term) => {
-    console.log(term);
+  getTerm = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization:
+          'Client-ID c67e1fe9281d943f4ed47c6f5068097bb79317daea4cee5ffdc705d0e620d416',
+      },
+    });
+    this.setState({ images: response.data.results });
+    console.log(response.data.results);
   };
   state = { images: [] };
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar term={this.getTerm} />
+        Found : {this.state.images.length}
       </div>
     );
   }
