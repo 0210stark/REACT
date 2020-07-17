024b779dc84ad7a9eac59670676fe116414a556c
Vidit Shah <vidit0210@outlook.com>
2020-07-18 01:24:16 +0530

    imageref

--- pics2/src/components/ImageCard.jsx
+++ pics2/src/components/ImageCard.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
   state = {};
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     return (
       <div>
         <img
+          ref={this.imageRef}
           alt={this.props.image.description}
           src={this.props.image.urls.regular}
         />
