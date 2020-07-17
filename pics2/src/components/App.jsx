574114efc6aee9e222e1389564f80bb0a510c5fd
Vidit Shah <vidit0210@outlook.com>
2020-07-17 23:43:38 +0530

    axios

--- pics2/src/components/App.jsx
+++ pics2/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
-import axios from axios;
 class App extends Component {
   getTerm = (term) => {
     console.log(term);
