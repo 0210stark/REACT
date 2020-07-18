e226c9f4f558d134dcccf56aa47dc32bc1bd110b
Vidit Shah <vidit0210@outlook.com>
2020-07-18 16:40:26 +0530

    commit

--- videos1/src/Components/App.jsx
+++ videos1/src/Components/App.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../api/youtube'
+import youtube from '../api/youtube';
+
 const KEY = 'AIzaSyDl_tkYkWptJsX5oFCHmUGSz-7J19qVHUQ';
 
 class App extends Component {
-   onTermSubmit = async term => {
-    const response = await youtube.get("/search", {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         q: term,
-        part: "snippet",
+        part: 'snippet',
         maxResults: 5,
         type: 'video',
-        key: KEY
-      }
+        key: KEY,
+      },
     });
+  };
+
   state = {};
   render() {
     return (
