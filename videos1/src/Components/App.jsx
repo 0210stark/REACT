bf84c87fc2781464e14242d6093d4edd5938d507
Vidit Shah <vidit0210@outlook.com>
2020-07-19 15:08:50 +0530

    v3

--- videos1/src/Components/App.jsx
+++ videos1/src/Components/App.jsx
@@ -5,6 +5,8 @@ import youtube from '../api/youtube';
 const KEY = 'AIzaSyDl_tkYkWptJsX5oFCHmUGSz-7J19qVHUQ';
 
 class App extends Component {
+  state = { videos: [] };
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
@@ -15,13 +17,14 @@ class App extends Component {
         key: KEY,
       },
     });
+    this.setState({ videos: response.data.items });
   };
 
-  state = {};
   render() {
     return (
       <div className='ui container' style={{ marginTop: '20px' }}>
         <SearchBar getTerm={this.onTermSubmit} />
+        <h2>I have {this.state.videos.length}</h2>
       </div>
     );
   }
