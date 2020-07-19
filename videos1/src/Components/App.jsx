229426e87346f7c694518b6c1643a04356770657
Vidit Shah <vidit0210@outlook.com>
2020-07-19 17:39:08 +0530

    v2

--- videos1/src/Components/App.jsx
+++ videos1/src/Components/App.jsx
@@ -19,9 +19,14 @@ class App extends Component {
         key: KEY,
       },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
-
+  componentDidMount() {
+    this.onTermSubmit('spacex');
+  }
   onVideoSelect = (video) => {
     this.setState({ selectedVideo: video });
   };
@@ -30,11 +35,19 @@ class App extends Component {
     return (
       <div className='ui container' style={{ marginTop: '20px' }}>
         <SearchBar getTerm={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
