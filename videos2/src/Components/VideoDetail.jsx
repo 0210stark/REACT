4e858d86e3c3fe9e4fba728a9083b42e9878cedc
Vidit Shah <vidit0210@outlook.com>
2020-07-20 14:59:37 +0530

    videoSrc

--- videos2/src/Components/VideoDetail.jsx
+++ videos2/src/Components/VideoDetail.jsx
@@ -7,7 +7,9 @@ const VideoDetail = ({ video }) => {
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
     <div>
-      <div className='ui embed'></div>
+      <div className='ui embed'>
+        <iframe src={videoSrc} />
+      </div>
       <div className='ui segment'>
         <h4 className='ui header'>{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
