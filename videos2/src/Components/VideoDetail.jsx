9bf075a273bddfb5134ca76905bdd7544355e0c3
Vidit Shah <vidit0210@outlook.com>
2020-07-20 14:58:22 +0530

    embed

--- videos2/src/Components/VideoDetail.jsx
+++ videos2/src/Components/VideoDetail.jsx
@@ -4,10 +4,14 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <h3>Loading..</h3>;
   }
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
-    <div className='ui segment'>
-      <h4 className='ui header'>{video.snippet.title}</h4>
-      <p>{video.snippet.description}</p>
+    <div>
+      <div className='ui embed'></div>
+      <div className='ui segment'>
+        <h4 className='ui header'>{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
     </div>
   );
 };
