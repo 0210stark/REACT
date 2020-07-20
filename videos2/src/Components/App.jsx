7cd436afe781830e65e99f764bbb116f7bbb9b90
Vidit Shah <vidit0210@outlook.com>
2020-07-20 12:55:59 +0530

    key

--- videos2/src/Components/App.jsx
+++ videos2/src/Components/App.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
+import youtube from '../api/youtube'
+
+const KEY = 'AIzaSyDl_tkYkWptJsX5oFCHmUGSz-7J19qVHUQ';
 class App extends Component {
-  state = {}
+  state = {videos}
   
+  getTermAndSearches = async (video) => {
+   const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY,
+      },
+    });
+}
+
   render() { 
     return (  );
   }
