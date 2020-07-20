bb3f6096124c869370fc5ac4a87af62ffb51e744
Vidit Shah <vidit0210@outlook.com>
2020-07-20 16:16:08 +0530

    v3

---
+++ videos3/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import youtube from '../api/youtube';
+import SearchBar from './SearchBar';
+
+class App extends Component {
+  state = { videos: [], selectedVideo: null }
+  
+
+
+  render() { 
+    return (  );
+  }
+}
+ 
+export default App;
---
+++ videos3/src/components/SearchBar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+class SearchBar extends Component {
+  state = { term: '' };
+  render() {
+    return (
+      <div className='ui segment'>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className='ui field'>
+            <label>Vidoe Search</label>
+            <input
+              type='text'
+              placeholder='search'
+              value={this.state.term}
+              onChange={(e) => {
+                this.setState({ term: e.target.value });
+              }}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
