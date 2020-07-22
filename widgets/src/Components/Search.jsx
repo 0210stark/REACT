af35eabc735a2b0baba33c48d7e7d17261f3cb57
Vidit Shah <vidit0210@outlook.com>
2020-07-22 12:14:19 +0530

    dshtml

--- widgets/src/Components/Search.jsx
+++ widgets/src/Components/Search.jsx
@@ -3,19 +3,51 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
 
-  useEffect(() => {}, [term]);
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className='item'>
+        <div className='content'>
+          <div className='header'>{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+          {/* {result.snippet} */}
+        </div>
+      </div>
+    );
+  });
 
   return (
-    <div className='ui form'>
-      <div className='field'>
-        <label>Enter search term</label>
-        <input
-          className='input'
-          value={term}
-          onChange={(e) => setTerm(e.target.value)}
-        />
+    <div>
+      <div className='ui form'>
+        <div className='field'>
+          <label>Enter search term</label>
+          <input
+            className='input'
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
       </div>
+      <div className='ui celled list'>{renderedResults}</div>
     </div>
   );
 };
