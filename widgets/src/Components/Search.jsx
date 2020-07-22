58b01dca20556ed4fd26b30b656bb9b7cf496578
Vidit Shah <vidit0210@outlook.com>
2020-07-22 11:47:59 +0530

    useEffect

--- widgets/src/Components/Search.jsx
+++ widgets/src/Components/Search.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+
+  useEffect(() => {}, []);
 
   return (
     <div className='ui form'>
